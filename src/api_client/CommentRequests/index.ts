ba849d621705b170c6325a7ff50830ccba8a1615
Nika Ts <nika.tseleva@mail.ru>
2024-03-27 05:26:29 +0400

    refactoring

--- src/api_client/CommentRequests/index.ts
+++ src/api_client/CommentRequests/index.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import {IComment} from "./type";
-import {ILike} from "../LikeRequests/type";
 
 class CommentRequests{
 
@@ -28,43 +27,6 @@ class CommentRequests{
         return {
             status: response.status,
             data: undefined,
-        }
-    }
-
-//like
-    async getLikes(itemId:string) {
-
-        const response = await axios({method: 'get', url: `/api/likes/${itemId}`});
-        return {
-            status: response.status,
-            data: response.data,
-        }
-    }
-
-    async addLike(like: ILike) {
-
-        const response = await axios({method: 'post', url: '/api/likes', data: {like: {...like}}});
-        return {
-            status: response.status,
-            data: undefined,
-        }
-    }
-
-    async deleteLike(likeId: string) {
-
-        const response = await axios({method: 'delete', url: `/api/likes`, data: {likeId:likeId}});
-        return {
-            status: response.status,
-            data: undefined,
-        }
-
-    }
-
-    async getSearchResult(searchValue:string) {
-        const response = await axios({method: 'get', url: `/api/search?value=${searchValue}`});
-        return {
-            status: response.status,
-            data: response.data,
         }
     }
 }
